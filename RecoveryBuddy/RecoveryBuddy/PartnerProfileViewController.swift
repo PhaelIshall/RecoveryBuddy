c7746494bb62230b6817b78e9a618f015cb5d7be
Wiem Ben Rim <Wiem@Wiems-MacBook-Pro.local>
2016-04-25 15:18:53 +0300

    Profile adjusted and partner profileand goals added

---
+++ RecoveryBuddy/RecoveryBuddy/PartnerProfileViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MilestonesViewController.swift
+//  RecoveryBuddy
+//
+//  Created by Wiem Ben Rim on 3/14/16.
+//  Copyright (c) 2016 Wiem Ben Rim. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class PartnerProfileViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        
+        
+    }
+    
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
+
---
+++ RecoveryBuddy/RecoveryBuddy/PushNotificationController.swift
@@ -0,0 +1,9 @@
+//
+//  PushNotificationController.swift
+//  RecoveryBuddy
+//
+//  Created by Wiem Ben Rim on 4/25/16.
+//  Copyright © 2016 Wiem Ben Rim. All rights reserved.
+//
+
+import Foundation
